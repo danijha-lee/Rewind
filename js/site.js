99cd1be985cbf7b830375d1eaf54cbea9cb46b90
danijha-lee <84164688+danijha-lee@users.noreply.github.com>
2021-10-10 03:10:58 -0400

    done

--- js/site.js
+++ js/site.js
@@ -1,31 +1,51 @@
-function displayMessage() {
-    let msg = "";
+function getString() {
 
-    msg = document.getElementById("message").value;
+    //Get the user's input
+    let userString = document.getElementById("userString").value;
 
-    alert(msg);
+
+    // if (userString != 2) {
+    //    Swal.fire({
+    //        icon: 'error',
+    //        title: 'Oops...',
+    //        text: 'You must enter 2 or more characters',
+           
+    //    })
+    // };
+
+    //Test the user's input for length
+    //Reverse the input => seperate function
+    let revString = reverseString(userString);
+
+    //Display the result => seperate function
+    displayString(revString);
+
 }
 
+function reverseString(userString) {
 
-function displayMessage2() {
-    let msg = "";
+    //Start at the last index
+    let startValue = userString.length - 1;
+    let endValue = 0;
 
-    msg = document.getElementById("message").value;
+    let revString = "";
 
-    Swal.fire(msg);
+    //In a decremented for loop, the start value must be higher than the end value
+    for (let i = startValue; i >= endValue; i--) {
+        revString += userString[i];
+    };
+
+    return revString
+
 }
 
+function displayString(revString) {
 
-// Two functions
-// function displayMessage(msg) {
+    return  Swal.fire({
+        icon: 'success',
+        title: 'Your Reversed String is:',
+        text: `${revString}`,
+    
+    })
 
-//     alert(msg);
-// }
-
-// function getMessage() {
-//     let msg = "";
-
-//     msg = document.getElementById("message").value;
-
-//     displayMessage(msg);
-// }
+}
